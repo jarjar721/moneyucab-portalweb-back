482247e4008dcbb7ecf464ad4493521137c45cd5
José Andrés Rodríguez <jose@crewnew.com>
2020-04-20 17:07:04 -0400

    initial commit

---
+++ moneyucab-portalweb-back/Models/MoneyUCABWebAPIContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace moneyucab_portalweb_back.Models
+{
+    public class MoneyUCABWebAPIContext: DbContext
+    {
+        public MoneyUCABWebAPIContext(DbContextOptions<MoneyUCABWebAPIContext> options):base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
