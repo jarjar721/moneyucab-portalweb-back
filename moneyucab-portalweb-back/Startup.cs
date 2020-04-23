ca9a3fc3b0db7f4ab4eaecc4e41efef0e0143ec4
José Andrés Rodríguez <jose@crewnew.com>
2020-04-23 02:14:26 -0400

    Web API connected to frontend!

--- moneyucab-portalweb-back/Startup.cs
+++ moneyucab-portalweb-back/Startup.cs
@@ -46,6 +46,8 @@ namespace moneyucab_portalweb_back
                 options.Password.RequireUppercase = false;
             }
             );
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +57,12 @@ namespace moneyucab_portalweb_back
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+            );
 
             app.UseAuthentication();
 
