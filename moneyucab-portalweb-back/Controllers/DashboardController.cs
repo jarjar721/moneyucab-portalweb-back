8b0a5473cdc5779c6b983168d09a4e17d8cfb9a9
José Andrés Rodríguez <jose@crewnew.com>
2020-04-23 15:39:31 -0400

    Token Authorized GET to Dashboard works!

--- moneyucab-portalweb-back/Controllers/DashboardController.cs
+++ moneyucab-portalweb-back/Controllers/DashboardController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using moneyucab_portalweb_back.Models.Entities;
 
 namespace moneyucab_portalweb_back.Controllers
 {
@@ -12,12 +14,27 @@ namespace moneyucab_portalweb_back.Controllers
     [ApiController]
     public class DashboardController : ControllerBase
     {
+        private UserManager<Usuario> _userManager;
+
+        public DashboardController(UserManager<Usuario> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         [Authorize]
         //GET: /api/Dashbard
         public async Task<Object> GetDashboard() 
         {
             string userId = User.Claims.First(c => c.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+
+            return new
+            {
+                user.UserName
+                //aqui se agregan ls campos que quieres devolver al front
+            };
+
         }
 
     }
