cf92920d66600039719506d116615d097215f6cd
jose romero <jaromero.16@est.ucab.edu.ve>
2020-06-17 19:18:11 -0400

    RF 5 funcional

--- moneyucab-portalweb-back/Entities/Saldo.cs
+++ moneyucab-portalweb-back/Entities/Saldo.cs
@@ -11,15 +11,6 @@ namespace moneyucab_portalweb_back.Entities
 		public Double saldoEnCuenta { get; set; }
 		public int idUsuario { get; set; }
 
-		public string JSONconvert()
-		{
-			//paso todos los datos
-			Saldo saldo = new Saldo();
-			saldo.saldoEnCuenta = this.saldoEnCuenta;
-
-			//devuelvo el string
-			string json = JsonConvert.SerializeObject(saldo);
-			return json;
-		}
+		
 	}
 }
