738049589cd0e9074ec18a96082844f26f8a77a9
jose romero <jaromero.16@est.ucab.edu.ve>
2020-06-21 22:21:24 -0400

    Listo Realizar cobro, falta consultar cobros

---
+++ moneyucab-portalweb-back/Comandos/ComandosService/Utilidades/Comando_Realizar_Cobro.cs
@@ -0,0 +1,32 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace moneyucab_portalweb_back.Comandos.ComandosService.Utilidades
+{
+	public class Comando_Realizar_Cobro
+	{
+		public int idUsuarioSolicitante { get; set; }
+		public String emailPagador { get; set; }
+		public int monto { get; set; }
+
+		public Boolean cobroRealizado = false;
+
+		public Comando_Realizar_Cobro(int _idUsuarioSolicitante,String _emailPagador,int _monto)
+		{
+			idUsuarioSolicitante = _idUsuarioSolicitante;
+			emailPagador = _emailPagador;
+			monto = _monto;
+		}
+
+		async public Task<Boolean> Ejecutar()
+		{
+			DAOBase dao = new DAOBase();
+			dao.Cobro(idUsuarioSolicitante, emailPagador, monto);
+			cobroRealizado = true;
+			return true;
+		}
+	}
+}
---
+++ moneyucab-portalweb-back/Controllers/CobroController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using moneyucab_portalweb_back.Comandos.ComandosService.Utilidades;
+using moneyucab_portalweb_back.Entities;
+
+namespace moneyucab_portalweb_back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CobroController : ControllerBase
+    {
+        [HttpPost]
+        [Route("RealizarCobro")]
+        public IActionResult Cuenta([FromBody]Cobro cobro) //No estoy claro de si aca se usa [frombody] o [fromform]
+        {
+
+
+            try
+            {
+                Comando_Realizar_Cobro comando_Realizar_Cobro = new Comando_Realizar_Cobro(cobro.idUsuarioSolicitante, cobro.emailPagador, cobro.monto);
+                comando_Realizar_Cobro.Ejecutar();
+                var resultado = comando_Realizar_Cobro.cobroRealizado;
+
+                return Ok(resultado);
+
+            }
+            catch (Exception error)
+            {
+                return BadRequest(false);
+            }
+
+
+        }
+    }
+}
